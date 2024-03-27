fa0e4b04b7dede75d62515344bcd8ed27d80b7c7
bryanathll <bryannatanael77@gmail.com>
2024-03-28 04:45:18 +0700

    InitialCommit

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,20 @@
 import {sum} from "../src/sum.js";
 
-test("sum(1, 2) must be 3",()=>{
+test("test sum function",()=>{
     const result = sum(1, 2)
 
     expect(result).toBe(3);
 });
+ 
+test("test sum function 2",()=>{
+    const result = sum(3, 2)
+
+    expect(result).toBe(5);
+});
+ 
+test("test sum function 3",()=>{
+    const result = sum(3, 5)
+
+    expect(result).toBe(8);
+});
+ 
